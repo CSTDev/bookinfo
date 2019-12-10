88cbca4643fcaedeb2b2e0bfa391016329c0f818
Carl Taylor <carl.taylor@triad.co.uk>
2019-12-10 16:03:02 +0000

    Add tekton pipeline definitions

---
+++ build-helper.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+IMAGE_URL=$1
+HASH=`git rev-parse --short HEAD`
+mkdir pipelineruns
+git fetch --depth=2 origin
+directories=`git diff --dirstat=files,0 HEAD~1 | sed 's/[0-9.]\+% //g' | uniq`
+for dir in $directories; do
+  echo $dir
+  SERVICE=`basename $dir`
+  echo $service
+  file=`find $dir -maxdepth 1 -name "pipeline-run.yaml"`
+  if [ $file ]; then
+    echo "got a pipeline run will deploy"
+    yq w -i $file "spec.resources[1].resourceSpec.params[0].value" $IMAGE_URL/$SERVICE:$HASH
+    yq w -i $file "metadata.name" $SERVICE-pipeline-run-$HASH-$RANDOM
+    cat $file
+    kubectl apply -f $file   
+  else
+    echo "no pipeline-run file found"
+  fi
+done
