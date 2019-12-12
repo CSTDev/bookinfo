2c40b4819d30fb56eabe2db02f4d5ed09a42aa58
Carl Taylor <carl.taylor@triad.co.uk>
2019-12-12 14:00:03 +0000

    Apply changes in the tekton directory

--- build-helper.sh
+++ build-helper.sh
@@ -3,9 +3,14 @@ IMAGE_URL=$1
 HASH=`git rev-parse --short HEAD`
 mkdir pipelineruns
 git fetch --depth=2 origin
-directories=`git diff --dirstat=files,0 HEAD~1 | sed 's/[0-9.]\+% //g' | uniq`
+directories=`git diff --dirstat=files,0 HEAD~1 | sed 's/[0-9.]\+% //g' | uniq | sort -r`
 for dir in $directories; do
   echo $dir
+  if [[ $dir == *'tekton'* ]]; then
+    echo "applying tekton resources"
+    kubectl apply -f $dir
+    continue
+  fi
   SERVICE=`basename $dir`
   echo $service
   file=`find $dir -maxdepth 1 -name "pipeline-run.yaml"`
